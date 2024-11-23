d8b4285ff319c6171de1462eb1ebc9252214b5d1
phulin <patrick.hulin@gmail.com>
2024-11-23 18:32:01 +0000

    Release for commit 1c5a6cc848bf4f4fefa4bd1f215bfa0a21eaa425: remove comment.

--- relay/choice.1485.js
+++ relay/choice.1485.js
@@ -7,10 +7,7 @@ function main(pageText) {
   const snippet = `<script id="snippet">${fileToBuffer(
     "data/high-speed-rail-load.js",
   )}</script>`;
-  write(
-    urlDecode(pageText).replace("</body>", snippet + "</body>"),
-    // .replace('<div id="fml"', '<div id="fml" style="display: none;"'),
-  );
+  write(urlDecode(pageText).replace("</body>", snippet + "</body>"));
 }
 
 module.exports.main = main;
